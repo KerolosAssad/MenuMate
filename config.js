17797bc9254be20f70f50e52d821809ba3bc8d58
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2025-04-20 17:02:01 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Automatically switches between local and tunnel depending on domain
 const API_BASE_URL = location.hostname.includes("localhost")
   ? "http://localhost:5000"
-  : "https://spotty-ravens-exist.loca.lt";
+  : "https://busy-ants-wink.loca.lt";
