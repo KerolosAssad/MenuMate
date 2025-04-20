bf3c59688b189a4fc57f45d24834729056187f6a
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2025-04-20 16:53:53 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = "https://spotty-ravens-exist.loca.lt"; // 🌐 Your public tunnel
-
-// const API_BASE_URL = "http://localhost:5000";
+// Automatically switches between local and tunnel depending on domain
+const API_BASE_URL = location.hostname.includes("localhost")
+  ? "http://localhost:5000"
+  : "https://spotty-ravens-exist.loca.lt";
