4574ab32182e6f4220c998cbd0abea70d211a4aa
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2025-04-21 19:54:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Automatically switches between local and tunnel depending on domain
 const API_BASE_URL = location.hostname.includes("localhost")
   ? "http://localhost:5000"
-  : "https://sharp-ghosts-mix.loca.lt";
+  : "https://two-toys-reply.loca.lt";
