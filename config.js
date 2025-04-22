50209b76494247c3467b2461397576f42cb78ed3
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2025-04-22 15:19:46 -0400

    Update config.js with new local tunneling link

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Automatically switches between local and tunnel depending on domain
 const API_BASE_URL = location.hostname.includes("localhost")
   ? "http://localhost:5000"
-  : "https://two-toys-reply.loca.lt";
+  : "https://ninety-ants-obey.loca.lt";
