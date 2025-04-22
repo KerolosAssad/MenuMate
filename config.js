7f0ecc421c2505877ad33e048b000fab89b1a055
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2025-04-22 16:07:15 -0400

    Update config.js with new local tunneling link

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Automatically switches between local and tunnel depending on domain
 const API_BASE_URL = location.hostname.includes("localhost")
   ? "http://localhost:5000"
-  : "https://ninety-ants-obey.loca.lt";
+  : "https://proud-bears-look.loca.lt";
