642b67502b6aca9d1d82bf0b8a63c9a19ab89a53
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2025-04-26 12:51:41 -0400

    Update config.js with new local tunneling link

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Automatically switches between local and tunnel depending on domain
 const API_BASE_URL = location.hostname.includes("localhost")
   ? "http://localhost:5000"
-  : "https://modern-cooks-punch.loca.lt";
+  : "https://smart-tips-attend.loca.lt";
