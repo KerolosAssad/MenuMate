34c58eaebaa36dcec74962b22bdb076b87f76ba2
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2025-04-26 14:01:54 -0400

    Update config.js with new local tunneling link

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Automatically switches between local and tunnel depending on domain
 const API_BASE_URL = location.hostname.includes("localhost")
   ? "http://localhost:5000"
-  : "https://forty-drinks-itch.loca.lt";
+  : "https://better-emus-greet.loca.lt";
