0d3860a65b461a07babb184734642b3a583b990a
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2025-04-26 17:54:03 -0400

    Update config.js with new render.com host link

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 // Automatically switches between local and tunnel depending on domain
-const API_BASE_URL = location.hostname.includes("localhost")
+/*const API_BASE_URL = location.hostname.includes("localhost")
   ? "http://localhost:5000"
   : "https://tame-pots-turn.loca.lt";
+*/
+
+const API_BASE_URL = "https://menumate-backend.onrender.com"; 
