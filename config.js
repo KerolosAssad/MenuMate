938e994420434632d681163cb98c0e0f6ab93825
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2025-04-26 03:41:49 -0400

    Update config.js with new local tunneling link

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Automatically switches between local and tunnel depending on domain
 const API_BASE_URL = location.hostname.includes("localhost")
   ? "http://localhost:5000"
-  : "https://violet-candies-shine.loca.lt";
+  : "https://few-bugs-notice.loca.lt";
