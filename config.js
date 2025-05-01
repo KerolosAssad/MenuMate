bda571f887f5bdf4f4059a1ab228202ba3cd8527
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2025-04-30 20:24:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// Automatically switches between local and tunnel depending on domain
-/*const API_BASE_URL = location.hostname.includes("localhost")
+//Automatically switches between local and tunnel depending on domain
+const API_BASE_URL = location.hostname.includes("localhost")
   ? "http://localhost:5000"
-  : "https://tame-pots-turn.loca.lt";
-*/
+  : "https://menumate-dev.loca.lt";
 
-const API_BASE_URL = "https://menumate-backend.onrender.com"; 
+
+//const API_BASE_URL = "https://menumate-backend.onrender.com"; 
